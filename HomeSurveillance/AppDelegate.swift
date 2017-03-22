d60756677f65e64a689659eaae7d1e99367f2ebe
Sapozhnik Ivan <sapozhnik.ivan@gmail.com>
2017-03-22 20:45:53 +0100

    initial commit

---
+++ HomeSurveillance/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  HomeSurveillance
+//
+//  Created by Sapozhnik Ivan on 22/03/17.
+//  Copyright © 2017 Sapozhnik Ivan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
