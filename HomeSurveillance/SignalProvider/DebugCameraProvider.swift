1ca464ea00da98e19c4da6a6b8109d1135e22327
Sapozhnik Ivan <sapozhnik.ivan@gmail.com>
2017-03-23 09:09:20 +0100

    Tests fixed for debug mode

---
+++ HomeSurveillance/SignalProvider/DebugCameraProvider.swift
@@ -0,0 +1,25 @@
+//
+//  DebugCameraProvider.swift
+//  HomeSurveillance
+//
+//  Created by Sapozhnik Ivan on 23/03/17.
+//  Copyright © 2017 Sapozhnik Ivan. All rights reserved.
+//
+
+import Cocoa
+import AVFoundation
+
+class DebugCameraProvider: SignalProviderItem {
+    
+    static func provider(withCaptureDevice device: AVCaptureDevice) -> SignalProviderItem {
+        return DebugCameraProvider()
+    }
+    
+    func startPreview() -> Bool {
+        return true
+    }
+    
+    func name() -> String {
+        return "Default name"
+    }
+}
